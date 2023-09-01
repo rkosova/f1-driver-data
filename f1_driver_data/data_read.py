805c5921eb9bfcac24d2219a29f736cf5bbee57b
Ron Kosova <ron.kosova@gmail.com>
2023-09-01 12:59:04 +0300

    read all data util

---
+++ f1_driver_data/data_read.py
@@ -0,0 +1,30 @@
+from .Driver import Driver
+import csv
+
+def read_all():
+    drivers = []
+
+    with open('f1_driver_data/data/F1Drivers_Dataset.csv') as f:
+        reader = csv.reader(f, delimiter=',')
+        for l in reader:
+    
+            drivers.append(Driver(
+                l[0],
+                l[1],
+                l[2],
+                l[3],
+                l[4],
+                l[5],
+                l[6],
+                l[7],
+                l[8]
+            ))
+    
+    return drivers
+
+
+if __name__=="__main__":
+    for i in read_all():
+        print(i)
+            
+
