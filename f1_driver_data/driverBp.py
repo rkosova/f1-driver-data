5a1e2ad42534fb9ff52d97302a7ac85ecc917568
Kjell Coppin <slmFKNshady@gmail.com>
2023-09-01 12:44:45 +0200

    added route to query single driver

--- f1_driver_data/driverBp.py
+++ f1_driver_data/driverBp.py
@@ -12,3 +12,13 @@ def driver_all():
     drivers_all = read_all()
     dict_drivers = [o.get_dict() for o in drivers_all]
     return jsonify(dict_drivers)
+
+@bp.route('/<driver_name>', methods=['GET'])
+def driver_by_name(driver_name):
+    drivers_all = read_all()
+    matching_drivers = [o.get_dict() for o in drivers_all if o.name == driver_name]
+    
+    if not matching_drivers:
+        return jsonify({'error': 'Driver not found'}), 404
+
+    return jsonify(matching_drivers[0])
