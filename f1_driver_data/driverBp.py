592c74ffd7ca7bdf8e7a0afc0534fe9308a6b2c8
Ron Kosova <ron.kosova@gmail.com>
2023-09-08 11:11:13 +0300

    Connected already existing API endpoint to /all with get parameters

--- f1_driver_data/driverBp.py
+++ f1_driver_data/driverBp.py
@@ -9,13 +9,23 @@ bp = Blueprint('driver', __name__, url_prefix='/driver')
 
 @bp.route('/all', methods=['GET'])
 def driver_all():
+    if request.args.get('driver_name'):
+        res = driver_by_name(request.args.get('driver_name'))
+        return res
+    elif request.args.get('ranking') == 'bottom':
+        n = request.args.get('n', type=int)
+        category = request.args.get('category')
+        res = bottom_n_drivers_by_category(number=n, category=category)
+        return res
+
+
     drivers_all = read_all()
     dict_drivers = [o.get_dict() for o in drivers_all]
     return jsonify(dict_drivers)
 
 
-@bp.route('/<driver_name>', methods=['GET'])
 def driver_by_name(driver_name):
+    print(driver_name)
     drivers_all = read_all()
     matching_drivers = [o.get_dict() for o in drivers_all if o.name == driver_name]
     
@@ -25,11 +35,8 @@ def driver_by_name(driver_name):
     return jsonify(matching_drivers[0])
 
 
-@bp.route('/bottom', methods=['GET'])
-def bottom_n_drivers_by_category():
+def bottom_n_drivers_by_category(category, number):
     # get query parameters
-    number = request.args.get('number', type=int)
-    category = request.args.get('category', type=str)
     if number is None or category is None:
         return jsonify({'error': 'Both "number" and "category" are required query parameters'}), 400
     
