1022e0e35466f72abe456a7396c5921c3ceb2209
Kjell Coppin <slmFKNshady@gmail.com>
2023-09-12 01:38:59 +0200

    updated driver blueprint for more logical implentation of /all endpoint

--- f1_driver_data/driverBp.py
+++ f1_driver_data/driverBp.py
@@ -9,30 +9,38 @@ bp = Blueprint('driver', __name__, url_prefix='/driver')
 
 @bp.route('/all', methods=['GET'])
 def driver_all():
-    if request.args.get('ranking') == 'bottom':
-        n = request.args.get('n', type=int)
-        category = request.args.get('category')
-        res = bottom_n_drivers_by_category(number=n, category=category)
-        return res
-
-
     drivers_all = read_all()
-    dict_drivers = [o.get_dict() for o in drivers_all]
+    dict_drivers = [driver.get_dict() for driver in drivers_all]
+    
+    if not request.args.get('category') and not request.args.get('ranking') and not request.args.get('n', type=int):
+        return jsonify(dict_drivers)
+
+    # filter category
+    if request.args.get('category') and request.args.get('ranking') and request.args.get('n', type=int):
+        category = request.args.get('category')
+        ranking = request.args.get('ranking')
+        n = request.args.get('n', type=int)
+
+        # check if category exists
+        if category not in dict_drivers[0]:
+            return jsonify({'error': f'Category "{category}" does not exist'}), 400
+
+        # Sort by given category and rank by bottom or top
+        if ranking == 'bottom':
+            dict_drivers.sort(key=lambda x: x[category])
+        elif ranking == 'top':
+            dict_drivers.sort(key=lambda x: x[category], reverse=True)
+        else:
+            return jsonify({'error': 'Invalid value for "ranking". It should be "top" or "bottom"'}), 400
+
+        # take n drivers
+        dict_drivers = dict_drivers[:n]
+
+    else:
+        return jsonify({'error': 'Both "n",  "category" and "ranking" are required query parameters'}), 400
+
     return jsonify(dict_drivers)
 
-def bottom_n_drivers_by_category(category, number):
-    # get query parameters
-    if number is None or category is None:
-        return jsonify({'error': 'Both "number" and "category" are required query parameters'}), 400
-    
-    # sort drivers by give category
-    drivers_all = read_all()
-    sorted_drivers = sorted(drivers_all, key=lambda driver: getattr(driver, category))
-
-    # Take the bottom 'n' drivers
-    bottom_n_drivers = [driver.get_dict() for driver in sorted_drivers[:number]]
-
-    return jsonify(bottom_n_drivers)
 
 @bp.route('/<driver_name>', methods=['GET'])
 def driver_by_name(driver_name):
