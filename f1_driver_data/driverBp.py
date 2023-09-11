5f0a1e2136674e21814cd38c2a1d033aab13e14c
Kjell Coppin <slmFKNshady@gmail.com>
2023-09-12 00:37:53 +0200

    separated get driver by name to own endpoint

--- f1_driver_data/driverBp.py
+++ f1_driver_data/driverBp.py
@@ -9,10 +9,7 @@ bp = Blueprint('driver', __name__, url_prefix='/driver')
 
 @bp.route('/all', methods=['GET'])
 def driver_all():
-    if request.args.get('driver_name'):
-        res = driver_by_name(request.args.get('driver_name'))
-        return res
-    elif request.args.get('ranking') == 'bottom':
+    if request.args.get('ranking') == 'bottom':
         n = request.args.get('n', type=int)
         category = request.args.get('category')
         res = bottom_n_drivers_by_category(number=n, category=category)
@@ -22,18 +19,6 @@ def driver_all():
     drivers_all = read_all()
     dict_drivers = [o.get_dict() for o in drivers_all]
     return jsonify(dict_drivers)
-
-
-def driver_by_name(driver_name):
-    print(driver_name)
-    drivers_all = read_all()
-    matching_drivers = [o.get_dict() for o in drivers_all if o.name == driver_name]
-    
-    if not matching_drivers:
-        return jsonify({'error': 'Driver not found'}), 404
-
-    return jsonify(matching_drivers[0])
-
 
 def bottom_n_drivers_by_category(category, number):
     # get query parameters
@@ -48,3 +33,13 @@ def bottom_n_drivers_by_category(category, number):
     bottom_n_drivers = [driver.get_dict() for driver in sorted_drivers[:number]]
 
     return jsonify(bottom_n_drivers)
+
+@bp.route('/<driver_name>', methods=['GET'])
+def driver_by_name(driver_name):
+    drivers_all = read_all()
+    matching_drivers = [o.get_dict() for o in drivers_all if o.name == driver_name]
+    
+    if not matching_drivers:
+        return jsonify({'error': 'Driver not found'}), 404
+
+    return jsonify(matching_drivers[0])
