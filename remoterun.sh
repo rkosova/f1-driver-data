91a56bd0305af628dd7302989aaa549dd99bb268
Ron Kosova <ron.kosova@gmail.com>
2023-10-06 13:19:52 +0300

    Added remoterun.sh bash script, used to pull and run run image/container from remote registry

---
+++ remoterun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REGISTRY_IP="10.0.40.160"
+PORT="5000"
+
+IMAGE_NAME="run_image_f1_dd"
+
+FULL_IMAGE_REF="${REGISTRY_IP}:${REGISTRY_PORT}/${IMAGE_NAME}:latest"
+
+echo "Pulling docker image: "
+docker pull "$FULL_IMAGE_REF"
+
+
+if [ $? -eq 0 ]; then
+  echo "Image successfully pulled: $FULL_IMAGE_REF"
+  NAME="F1DriverData_test"
+  docker run --name "$NAME" -d "$FULL_IMAGE_REF" 
+else
+  echo "Failed to pull the image: $FULL_IMAGE_REF"
+  exit 1
+fi
+
