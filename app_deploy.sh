acdbdf48eaca38fa15797a384dfc03ab6cd2fe3f
Ron Kosova <ron.kosova@gmail.com>
2023-10-13 13:41:03 +0300

    Testing remote run 19.11

--- app_deploy.sh
+++ app_deploy.sh
@@ -7,4 +7,8 @@ pip install F1DriverData-0.1-py3-none-any.whl
 pip install waitress
 pip install flask
 
+pip show F1DriverData
+python -c "import f1_driver_data"
+
+
 waitress-serve --call f1_driver_data:create_app
