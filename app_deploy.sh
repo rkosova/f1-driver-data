8f53550414550f75b293f68151bddcf9ffed1182
Ron Kosova <ron.kosova@gmail.com>
2023-10-13 13:35:05 +0300

    Testing remote run 19.10

--- app_deploy.sh
+++ app_deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 python -m venv venv
-source ./venv/bin/activate
+source venv/bin/activate
 ls
+pip install -r requirements.txt
 pip install F1DriverData-0.1-py3-none-any.whl
 pip install waitress
 pip install flask
 
-waitress-serve --call "f1_driver_data:create_app"
+waitress-serve --call f1_driver_data:create_app
