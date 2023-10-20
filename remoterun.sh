cb4f0542492fd6da35ae2df096275429585c9196
Ron Kosova <ron.kosova@gmail.com>
2023-10-20 14:15:57 +0300

    Removed extra docker run in remoterun.sh :,)

--- remoterun.sh
+++ remoterun.sh
@@ -14,7 +14,6 @@ if [ $? -eq 0 ]; then
   echo "Image successfully pulled: $FULL_IMAGE_REF"
   NAME="F1DriverData_test"
   docker run --name "$NAME" -p 9001:8080 -d "$FULL_IMAGE_REF" 
-  docker run --name "$NAME" -p 9001:8080
 else
   echo "Failed to pull the image: $FULL_IMAGE_REF"
   exit 1
